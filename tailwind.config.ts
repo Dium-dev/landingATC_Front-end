3d4a4834a979400d2781ecd8ff8fc372de533a6b
Telkens <stivenmg77@gmail.com>
2024-02-26 09:30:46 -0500

    Add Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,69 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/stories/**/*.{js,ts,jsx,tsx}',
+    './src/assets/**/*.{js,ts,jsx,tsx}'
+  ],
+    safelist: [
+    'xs:min-w-[calc(100%/3)]',
+    'xs:min-w-[calc(100%/5)]',
+    'ms:min-w-[calc(100%/3)]',
+    'ms:min-w-[calc(100%/4)]',
+    'ms:min-w-[calc(100%/5)]',
+    'md:min-w-[calc(100%/4)]',
+    'md:min-w-[calc(100%/5)]',
+    'lg:min-w-[calc(100%/5)]',
+    {
+        pattern: /min-w-./,
+        variants: ['xl', 'lg', 'md', 'ms', 'xs']
+    }
   ],
   theme: {
+    colors: {
+      'background-lm': '#f1f1f1',
+      'background-dm': '#13131d',
+      'primary-lm': '#fe0000',
+      'primary-dm': '#444444',
+      'secondary-lm': '#F20505',
+      'secondary-dm': '#555555',
+      'secondary-background': '#ffff',
+      'text-lm': '#333333',
+      'text-dm': '#f1f1f1',
+      'input-bg': '#FFF2F7',
+      white: '#fff',
+      black: '#000',
+      green: '#008000',
+      gray: '#DCDCDC'
+    },
+    screens: {
+      xs: '480px',
+      ms: '768px',
+      md: '1080px',
+      lg: '1200px',
+      xl: '1480px',
+      xxl: '1899px',
+      xxxl: '1920px'
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        banner:
+          'url("https://i0.wp.com/citymagazine.si/wp-content/uploads/2022/05/2023-audi-rs5-coupe-competition.jpg?fit=1920%2C1080&ssl=1")',
       },
+
+      maxWidth: {
+        'f-hd': '1920px',
+      },
+      minWidth: {
+        100: '100px',
+      },
+      gap: {
+        50: '50px',
+      },
+      plugins: [],
     },
   },
-  plugins: [],
 };
-export default config;
