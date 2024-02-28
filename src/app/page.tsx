06cc4fd0b56fd0b90a020db3f3386723e35821ec
Telkens <stivenmg77@gmail.com>
2024-02-28 11:56:40 -0500

    Add principal banner using a gallery image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Landing</h1>
+    <main>
+      <Banner />
     </main>
   );
 }
