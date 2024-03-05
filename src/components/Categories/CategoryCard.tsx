92ad2941c4d9d6625c382d084f523df63546e84e
Telkens <stivenmg77@gmail.com>
2024-03-05 15:47:51 -0500

    Fix category name text, it wasn´t centered

--- src/components/Categories/CategoryCard.tsx
+++ src/components/Categories/CategoryCard.tsx
@@ -9,13 +9,13 @@ interface CategoryCardProps {
 
 const CategoryCard: FC<CategoryCardProps> = ({ image, name }) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-x-4 relative group">
-      <Link href={"https://actualizatucarro.mercadoshops.com.co/"}>
+    <Link href={"https://actualizatucarro.mercadoshops.com.co/"}>
+      <div className="flex flex-col items-center justify-center gap-x-4 relative group">
         <Image src={image} width={150} height={150} alt={name} />
         <span className="font-bold text-sm md:text-lg">{name}</span>
         <div className="absolute h-1 bg-primary-lm bottom-[-5px] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-200 ease-in-out w-full rounded"></div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 export default CategoryCard;
