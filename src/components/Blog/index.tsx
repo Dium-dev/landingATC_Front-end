b784f2e0c1e3e352bf671374863af3cd9520342e
Telkens <stivenmg77@gmail.com>
2024-03-07 16:24:41 -0500

    Add images to banner blog component

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -4,14 +4,16 @@ import { FC } from "react";
 
 const Blog: FC = () => {
   return (
-    <section className="w-full bg-black">
-      <Link
-        href={"https://actualizatucarro.blogspot.com"}
-        className="bg-cover bg-center bg-no-repeat bg-fixed bg-banner flex items-center justify-center max-w-[1920px] mx-auto p-10"
-        target="_blank"
-      >
-        <span className="text-9xl text-white">BLOG</span>
-      </Link>
+    <section className='w-full bg-black'>
+    <Link className="bg-cover bg-no-repeat bg-fixed bg-banner flex items-center justify-center max-w-[1920px] mx-auto" href={'https://actualizatucarro.blogspot.com'} target='_blank'>
+      <Image
+        src={'/images/blog.webp'}
+        alt="Título del Blog"
+        width={668}
+        height={363}
+        loading="lazy"
+      />
+    </Link>
     </section>
   );
 };
