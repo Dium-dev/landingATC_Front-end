521f833f3971dc1d3cbc215ffc57e839d4c356de
Telkens <stivenmg77@gmail.com>
2024-03-07 18:43:21 -0500

    Fix font size at the category text in very small display size

--- src/components/Categories/CategoryCard.tsx
+++ src/components/Categories/CategoryCard.tsx
@@ -12,7 +12,7 @@ const CategoryCard: FC<CategoryCardProps> = ({ image, name }) => {
     <Link href={"https://actualizatucarro.mercadoshops.com.co/"}>
       <div className="flex flex-col items-center justify-center gap-x-4 relative group">
         <Image src={image} width={150} height={150} alt={name} />
-        <span className="font-bold text-sm md:text-lg">{name}</span>
+        <span className="font-bold text-xs ms:text-sm md:text-lg">{name}</span>
         <div className="absolute h-1 bg-primary-lm bottom-[-5px] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-200 ease-in-out w-full rounded"></div>
       </div>
     </Link>
