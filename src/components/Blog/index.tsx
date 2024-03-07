ddb4e8eed61df97c697ddf54e3d48cea246942b1
Telkens <stivenmg77@gmail.com>
2024-03-07 18:35:22 -0500

    Center background image

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -5,7 +5,7 @@ import { FC } from "react";
 const Blog: FC = () => {
   return (
     <section className='w-full bg-black'>
-    <Link className="bg-cover bg-no-repeat bg-fixed bg-banner flex items-center justify-center max-w-[1920px] mx-auto" href={'https://actualizatucarro.blogspot.com'} target='_blank'>
+    <Link className="bg-cover bg-center bg-no-repeat bg-fixed bg-banner flex items-center justify-center max-w-[1920px] mx-auto" href={'https://actualizatucarro.blogspot.com'} target='_blank'>
       <Image
         src={'/images/blog.webp'}
         alt="Título del Blog"
