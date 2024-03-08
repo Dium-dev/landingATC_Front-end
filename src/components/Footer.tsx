d814ea588b931c1b3b5a910d417dbd7f2e47d8bf
SebastianMartel <martel.ssebastian@gmail.com>
2024-03-07 21:52:43 -0500

    [FEAT]: Reimplementing old design, Footer structure achieved.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,7 +31,6 @@ function Instagram() {
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16">
             <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
         </svg>
-
     )
 }
 function Pinterest() {
@@ -42,79 +41,14 @@ function Pinterest() {
     )
 }
 
-function Whatsapp() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-whatsapp" viewBox="0 0 16 16">
-            <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
-        </svg>
-    )
-}
-
-function Mail() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16" style={{ verticalAlign: "middle" }}>
-            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
-        </svg>
-    )
-}
-
-interface LinkListProps {
-    array: any[];
-    hasIcons: boolean
-}
-function LinkList({ array, hasIcons }: LinkListProps) {
-    return (
-        <ul className={hasIcons ? "flex justify-between" : "flex flex-col gap-4"}>
-            {
-                array.map((item) => (
-                    hasIcons ? (
-                        <li key={item.name} className="grid place-items-center size-[32px] rounded-full border border-primary-lm">
-                            <item.icon />
-                        </li>
-                    ) : (
-                        <li key={item.name}>
-                            <a
-                                className="relative after:content-[''] after:absolute after:-bottom-0 after:left-0 after:w-full after:h-[2.5px] after:bg-secondary-lm after:scale-x-0 after:transform after:origin-bottom-right after:transition-transform hover:after:scale-x-100 hover:after:origin-bottom-left"
-                                href={`/${item.path}`}
-                            >
-                                {item.name}
-                            </a>
-                        </li>
-                    )
-                ))
-            }
-        </ul>
-    )
-}
-
 interface FooterProps { }
 function Footer({ }: FooterProps) {
 
-    const NAV_LINKS = {
-        categories: [
-            { name: "Farolas", path: "" },
-            { name: "Stops", path: "" },
-            { name: "Audio", path: "" },
-            { name: "Exploradoras", path: "" },
-            { name: "Exterior", path: "" },
-            { name: "Interior", path: "" },
-            { name: "Bombillos", path: "" },
-            { name: "Repuestos", path: "" }
-        ],
-        company: [
-            { name: "¿Quiénes somos?", path: "" },
-            { name: "Aviso legal", path: "" }
-        ],
-        community: [
-            { name: "Contáctanos", path: "" },
-            { name: "Soporte", path: "" },
-            { name: "Blog", path: "" }
-        ],
-        purchase: [
-            { name: "Mi cuenta", path: "" },
-            { name: "¿Cómo comprar?", path: "" }
-        ]
-    }
+    const NAV_LINKS = [
+        { name: 'Nosotros', path: '/aboutUs' },
+        { name: '¿Cómo comprar?', path: '/como-comprar' },
+        { name: 'Blog', path: 'https://actualizatucarro.blogspot.com/' }
+    ];
 
     const SOCIALMEDIA_LINKS = [
         { name: "Facebook", icon: Facebook, path: "" },
@@ -125,74 +59,63 @@ function Footer({ }: FooterProps) {
     ]
 
     return (
-        <footer className="flex flex-col items-center justify-center w-full px-[100px] pt-[100px] pb-[50px] bg-[#1C2023] text-white">
-            <div className="flex justify-between w-full max-w-[1920px] mb-[100px]">
-                {/* LOGO */}
-                <div className="flex flex-col items-center gap-8">
-                    <Image
-                        src={logo}
-                        alt="Actualiza tu carro logo."
-                        width={245}
-                        style={{ aspectRatio: '664 / 381' }}
-                    />
-                    <div className='w-full'>
-                        <div className="mb-4 text-[20px] font-bold">Síguenos</div>
-                        <LinkList array={SOCIALMEDIA_LINKS} hasIcons={true} />
-                    </div>
-                </div>
-                {/* LINKS */}
-                <div className="flex justify-between flex-1 px-[8vw]">
-                    <div>
-                        <div className="mb-4 text-[20px] font-bold">Categorías</div>
-                        <nav>
-                            <ul>
-                                <LinkList array={NAV_LINKS.categories} hasIcons={false} />
+        <footer className="flex flex-col items-center justify-center w-full px-[100px] pt-[100px] pb-[25px] bg-[#1C2023] text-white">
+            <div className="flex justify-between items-stretch w-full max-w-[1920px] mb-[100px]">
+                {/* LOGO AND SOCIAL MEDIA */}
+                <div className="flex gap-[100px]">
+                    <div className="flex flex-col items-center justify-between">
+                        <Image
+                            src={logo}
+                            alt="Actualiza tu carro logo."
+                            width={300}
+                            style={{ aspectRatio: '664 / 381' }}
+                        />
+                        <nav className='w-full'>
+                            <ul className="flex justify-between">
+                                {
+                                    SOCIALMEDIA_LINKS.map((item) => (
+                                        <li key={item.name} className="grid place-items-center size-[32px] rounded-full border border-primary-lm">
+                                            <item.icon />
+                                        </li>
+                                    ))
+                                }
                             </ul>
                         </nav>
                     </div>
-                    <div>
-                        <div className="mb-[24px]">
-                            <div className="mb-4 text-[20px] font-bold">Empresa</div>
-                            <nav>
-                                <LinkList array={NAV_LINKS.company} hasIcons={false} />
-                            </nav>
-                        </div>
-                        <div>
-                            <div className="mb-4 text-[20px] font-bold">Comunidad</div>
-                            <nav>
-                                <LinkList array={NAV_LINKS.community} hasIcons={false} />
-                            </nav>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="mb-4 text-[20px] font-bold">Compra</div>
-                        <nav>
-                            <LinkList array={NAV_LINKS.purchase} hasIcons={false} />
-                        </nav>
-                    </div>
+                    <div className="h-[240px] w-[1.5px] bg-white bg-opacity-[.1]" />
                 </div>
-                {/* CONTACT & PAYMENT METHODS */}
-                <div className="flex flex-col items-end gap-[50px]">
-                    <div className="flex xxxl:flex-row xl:flex-col items-end gap-[16px]">
-                        <div className="flex items-center gap-4 w-fit p-4 bg-white text-black">
-                            <Whatsapp />
-                            <span className="text-nowrap">+54 1234567891</span>
-                        </div>
-                        <div className="flex items-center gap-4 w-fit p-4 bg-white text-black">
-                            <Mail />
-                            <span className="text-nowrap">customersupport@emails.com</span>
-                        </div>
-                    </div>
+                {/* <div className="hidden md:flex w-[2px] h-60 bg-gradient-to-t from-background-dm via-white to-background-dm md:ml-16 "></div> */}
+                {/* LINKS, CONTACT AND PAYMENT METHODS */}
+                <div className="flex flex-1 flex-col items-center justify-between">
+                    <nav className="w-[498px]">
+                        <ul className="flex justify-between w-full">
+                            {
+                                NAV_LINKS.map((item) => (
+                                    <li key={item.name}>
+                                        <a
+                                            href={item.path}
+                                            className="relative inline-block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2.5px] after:bg-secondary-lm after:scale-x-0 after:transform after:origin-bottom-right after:transition-transform hover:after:scale-x-100 hover:after:origin-bottom-left"
+                                        >
+                                            {item.name}
+                                        </a>
+                                    </li>
+                                ))
+                            }
+                            <button className="relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2.5px] after:bg-secondary-lm after:scale-x-0 after:transform after:origin-bottom-right after:transition-transform hover:after:scale-x-100 hover:after:origin-bottom-left">
+                                Contacto
+                            </button>
+                        </ul>
+                    </nav>
                     <Image
                         src={paymentMethods}
                         alt="Métodos de pago: Mercado pago."
                         width={498}
                         style={{ aspectRatio: "257 / 73" }}
-                        className="xxxl:w-[498px] xl:w-[286px]"
+                        className=""
                     />
                 </div>
             </div>
-            <div className="w-full pt-[50px] max-w-[1920px] border-t border-t-white border-opacity-[.1] text-sm text-center">
+            <div className="w-full pt-[25px] max-w-[1920px] text-sm text-center">
                 ©Copyrigth 2024. Todos los derechos reservados - Desarrollado por: <strong>Work Team</strong>
             </div>
         </footer>
