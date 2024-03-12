e18f820493b801a0ca00b2aac02d87357d1dd9f6
Telkens <stivenmg77@gmail.com>
2024-03-12 09:58:34 -0500

    Add navigation to ATC e-shop page throught the Brand item

--- src/components/BrandsCarousel/BrandCard.tsx
+++ src/components/BrandsCarousel/BrandCard.tsx
@@ -1,14 +1,24 @@
 import { Brand } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
 
 interface BrandCardProps {
-  brand: Brand
+  brand: Brand;
 }
 
 export const BrandCard = ({ brand }: BrandCardProps) => {
   return (
-    <div className="flex items-center justify-center cursor-pointer w-max ms:m-6 mx-1 hover:scale-125 transition-all">
-      <Image src={brand.image} width={100} height={100} alt={brand.name} className="w-auto h-auto"/>
-    </div>
+    <Link
+      className="flex items-center justify-center cursor-pointer w-max ms:m-6 mx-1 hover:scale-125 transition-all"
+      href={"shop.actualizatucarro.com/"}
+    >
+      <Image
+        src={brand.image}
+        width={100}
+        height={100}
+        alt={brand.name}
+        className="w-auto h-auto"
+      />
+    </Link>
   );
 };
