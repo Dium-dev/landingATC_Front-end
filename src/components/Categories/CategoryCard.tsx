1df34f2a82515a75ccc047ecdf3fe853a1e1d3f6
Telkens <stivenmg77@gmail.com>
2024-03-15 14:24:20 -0500

    Add a transition scale-up to category image

--- src/components/Categories/CategoryCard.tsx
+++ src/components/Categories/CategoryCard.tsx
@@ -11,8 +11,8 @@ const CategoryCard: FC<CategoryCardProps> = ({ image, name }) => {
   return (
     <Link href={"https://actualizatucarro.mercadoshops.com.co/"}>
       <div className="flex flex-col items-center justify-center gap-x-4 relative group">
-        <Image src={image} width={150} height={150} alt={name} />
-        <span className="font-bold text-xs ms:text-sm md:text-lg">{name}</span>
+        <Image src={image} width={150} height={150} alt={name} className="group-hover:scale-125 transition-all"/>
+        <span className="font-bold text-xs ms:text-sm md:text-lg ms:group-hover:text-xl transition-all">{name}</span>
         <div className="absolute h-1 bg-primary-lm bottom-[-5px] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-200 ease-in-out w-full rounded"></div>
       </div>
     </Link>
