3284f026df90a1ccea60d9436e3343d62f5d089f
Telkens <stivenmg77@gmail.com>
2024-03-20 14:25:35 -0500

    Add onclick navigation functionality

--- src/components/AboutUsBanner/index.tsx
+++ src/components/AboutUsBanner/index.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const AboutUsBanner = () => {
   return (
     <section className="flex items-center justify-evenly w-full py-7 ms:py-12 gap-x-12 px-3 xs:px-8 md:px-4 lg:px-0 max-w-f-hd mx-auto">
       <div className="md:w-1/4 lg:w-1/3 space-y-2 ms:space-y-6 h-min">
         <div className="flex justify-between items-center gap-x-6">
-          <h2 className="text-2xl xs:text-4xl ms:text-5xl text-primary-lm font-bold">Quienes somos</h2>
-          <Image
-          src={"/images/roundLogo.webp"}
-          alt="Logo ATC"
-          width={85}
-          height={85}
-          className="md:hidden"
-        />
+          <h2 className="text-2xl xs:text-4xl ms:text-5xl text-primary-lm font-bold">
+            Quienes somos
+          </h2>
+          <Link href={"about"}>
+            <Image
+              src={"/images/roundLogo.webp"}
+              alt="Logo ATC"
+              width={85}
+              height={85}
+              className="md:hidden"
+            />
+          </Link>
         </div>
         <p className="text-sm xs:text-lg ms:text-xl md:text-base lg:text-2xl ml-4 ms:ml-10 md:ml-2 lg:ml-5">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aut
@@ -23,7 +28,12 @@ export const AboutUsBanner = () => {
           debitis sit et molestias est praesentium dolorum rem ipsam obcaecati
           veniam. Impedit, quia qui! Illum delectus repellendus eum hic
           recusandae neque rem commodi dolores suscipit soluta, expedita libero,
-          voluptatem sunt qui quidem quaerat.
+          voluptatem sunt qui quidem quaerat.{" "}
+          <Link href={"about"}>
+            <span className="text-primary-lm font-bold hover:underline">
+              Leer más...
+            </span>
+          </Link>
         </p>
       </div>
       <div className="relative hidden md:block">
@@ -33,13 +43,15 @@ export const AboutUsBanner = () => {
           width={650}
           height={650}
         />
-        <Image
-          src={"/images/roundLogo.webp"}
-          alt="Logo ATC"
-          width={180}
-          height={180}
-          className="absolute -left-20 -bottom-6"
-        />
+        <Link href={"about"}>
+          <Image
+            src={"/images/roundLogo.webp"}
+            alt="Logo ATC"
+            width={180}
+            height={180}
+            className="absolute -left-20 -bottom-6"
+          />
+        </Link>
       </div>
     </section>
   );
