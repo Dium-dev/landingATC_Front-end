96e6e598f6c21f24f8d4e175f5123961b47b12ca
Telkens <stivenmg77@gmail.com>
2024-03-20 14:47:02 -0500

    Fix brands number in mobile devices and reduce the gradient width

--- src/components/BrandsCarousel/BrandCard.tsx
+++ src/components/BrandsCarousel/BrandCard.tsx
@@ -1,12 +1,15 @@
 import { Brand } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
+import useMobile from "@/hooks/useMobile";
 
 interface BrandCardProps {
   brand: Brand;
 }
 
 export const BrandCard = ({ brand }: BrandCardProps) => {
+  const { isMobile } = useMobile();
+
   return (
     <Link
       className="flex items-center justify-center cursor-pointer w-max ms:m-6 mx-1 hover:scale-125 transition-all"
@@ -15,8 +18,8 @@ export const BrandCard = ({ brand }: BrandCardProps) => {
     >
       <Image
         src={brand.image}
-        width={100}
-        height={100}
+        width={isMobile ? 50 : 100}
+        height={isMobile ? 50 : 100}
         alt={brand.name}
         className="w-auto h-auto"
       />
