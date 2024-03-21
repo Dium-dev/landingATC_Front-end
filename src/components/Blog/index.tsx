dfdb67bf223f77ec4f046374a4a67a72384187f1
Telkens <stivenmg77@gmail.com>
2024-03-21 18:42:48 -0500

    Add change color in hover at the blog text image

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,19 +1,24 @@
+"use client";
+import { ReactSVG } from "react-svg";
 import Link from "next/link";
 import Image from "next/image";
 import { FC } from "react";
 
 const Blog: FC = () => {
   return (
-    <section className='w-full bg-black'>
-    <Link className="bg-cover bg-center bg-no-repeat bg-fixed bg-banner flex items-center justify-center max-w-[1920px] mx-auto px-4 ms:px-0" href={'https://actualizatucarro.blogspot.com'} target='_blank'>
-      <Image
-        src={'/images/blog.webp'}
-        alt="Título del Blog"
-        width={668}
-        height={363}
-        loading="lazy"
-      />
-    </Link>
+    <section className="w-full bg-black">
+      <Link
+        className="bg-cover bg-center bg-no-repeat bg-fixed bg-banner flex items-center justify-center max-w-[1920px] mx-auto px-4 ms:px-0"
+        href={"https://actualizatucarro.blogspot.com"}
+        target="_blank"
+      >
+        <ReactSVG
+          src="/icons/blogLogo.svg"
+          beforeInjection={(svg) => {
+            svg.classList.add("fil0");
+          }}
+        />
+      </Link>
     </section>
   );
 };
