32d179930a82655a5239487e3d4ba6824411512e
Telkens <stivenmg77@gmail.com>
2024-04-02 17:18:08 -0500

    Add a scale-up transition

--- src/components/WhatsAppButton/index.tsx
+++ src/components/WhatsAppButton/index.tsx
@@ -3,7 +3,7 @@ import { MdOutlineWhatsapp } from "react-icons/md";
 
 export const WhatsAppButton = () => {
   return (
-    <div className="w-20 h-20 rounded-full bg-[#25D366] fixed z-10 bottom-10 right-10 ms:flex items-center justify-center hidden cursor-pointer">
+    <div className="w-20 h-20 rounded-full bg-[#25D366] fixed z-10 bottom-10 right-10 ms:flex items-center justify-center hidden cursor-pointer hover:scale-125 transition-all">
       <Link href={'#'} className="text-white">
         <MdOutlineWhatsapp size={55} />
       </Link>
