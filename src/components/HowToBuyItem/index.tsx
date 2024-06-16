d3159e8e090022bdf6df7bedb0c14e7d5ba37659
Telkens <stivenmg77@gmail.com>
2024-06-16 14:34:00 -0500

    Fix import module error

--- src/components/HowToBuyItem/index.tsx
+++ src/components/HowToBuyItem/index.tsx
@@ -15,12 +15,7 @@ export const HowToBuyItem = ({
   background,
 }: HowToBuyItemProps) => {
   return (
-    <div
-      className={cn(
-        "flex flex-col lg:flex-row items-center justify-center lg:justify-evenly h-full gap-y-6",
-        `bg-[url("${background}")]`
-      )}
-    >
+    <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-evenly h-full gap-y-6">
       <Image src={icon} width={200} height={200} alt={title} />
       <div className="w-1/2 text-center lg:text-start">
         <h2 className="text-2xl font-bold">{title}</h2>
