ab95d04d83706f311d1fc9839c41f9498172eff2
Telkens <stivenmg77@gmail.com>
2024-09-09 08:31:18 -0500

    Fix wrong navbar path

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -16,7 +16,7 @@ import { PaymentMethods } from '@/components/PaymentMethods'
 import { ReviewsSkeleton } from "@/components/Reviews/ReviewsSkeleton";
 import { WhatsAppButton } from '@/components/WhatsAppButton'
 import { Suspense } from "react";
-import { NavBar } from './(nav-bar)'
+import { NavBar } from '../(nav-bar)'
 
 export default function Home() {
   return (
