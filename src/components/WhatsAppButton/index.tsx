73a1f647f0de10085662e4b3912e6500908877e6
Telkens <stivenmg77@gmail.com>
2024-11-27 08:19:11 -0500

    Fix button size

--- src/components/WhatsAppButton/index.tsx
+++ src/components/WhatsAppButton/index.tsx
@@ -3,9 +3,9 @@ import { MdOutlineWhatsapp } from "react-icons/md";
 
 export const WhatsAppButton = () => {
   return (
-    <div className="w-20 h-20 rounded-full bg-[#25D366] fixed z-10 bottom-10 right-10 flex items-center justify-center cursor-pointer hover:scale-125 transition-all">
+    <div className="w-16 h-16 rounded-full bg-[#25D366] fixed z-10 bottom-10 right-10 flex items-center justify-center cursor-pointer hover:scale-125 transition-all">
       <Link href={'https://wa.link/1rpg2h'} className="text-white" target="_blank">
-        <MdOutlineWhatsapp size={55} />
+        <MdOutlineWhatsapp size={47} />
       </Link>
     </div>
   );
